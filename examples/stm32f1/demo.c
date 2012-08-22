49bf1cb6e307cfa7f4cd24ab0caeb4dbdf150494
Karl Palsson <karlp@tweak.net.au>
2012-08-22 21:03:01 +0000

    Prints out the usart at least.
    Still need to review all the spi stuff, setup delay, and chip select doesn't seem right.

--- examples/stm32f1/demo.c
+++ examples/stm32f1/demo.c
@@ -1,21 +1,36 @@
 // Karl Palsson, 2011
 // false.ekta.is
 // BSD/MIT Licensed.
-// Keep netbeans happy
-#ifndef __AVR_ATmega32U4__
-#define __AVR_ATmega32U4__
-#endif
 
+#include <libopencm3/stm32/spi.h>
+#include <libopencm3/stm32/nvic.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/f1/gpio.h>
+#include <libopencm3/stm32/f1/rcc.h>
+
+#include <errno.h>
 #include <stdio.h>
 #include "simrf.h"
 #include "simrf_plat.h"
 
-void init(void) {
-    platform_simrf_init();
 
+int _write(int file, char *ptr, int len) {
+    int i;
+
+    if (file == 1) {
+        for (i = 0; i < len; i++)
+            usart_send_blocking(USART2, ptr[i]);
+        return i;
+    }
+    errno = EIO;
+    return -1;
+}
+
+void init(void) {
+
+    platform_simrf_init();
     // interrupt pin from mrf
     platform_mrf_interrupt_enable();
-
 }
 
 void handle_rx(simrf_rx_info_t *rxinfo, uint8_t *rx_buffer) {
@@ -23,11 +38,15 @@ void handle_rx(simrf_rx_info_t *rxinfo, uint8_t *rx_buffer) {
     printf("Received a packet: %u bytes long\n", rxinfo->frame_length);
     printf("headers:");
     switch (rxinfo->frame_type) {
-        case MAC_FRAME_TYPE_BEACON: printf("[ft:beacon]"); break;
-        case MAC_FRAME_TYPE_DATA: printf("[ft:data]"); break;
-        case MAC_FRAME_TYPE_ACK: printf("[ft:ack]"); break;
-        case MAC_FRAME_TYPE_MACCMD: printf("[ft:mac command]"); break;
-        default: printf("[ft:reserved]");
+    case MAC_FRAME_TYPE_BEACON: printf("[ft:beacon]");
+        break;
+    case MAC_FRAME_TYPE_DATA: printf("[ft:data]");
+        break;
+    case MAC_FRAME_TYPE_ACK: printf("[ft:ack]");
+        break;
+    case MAC_FRAME_TYPE_MACCMD: printf("[ft:mac command]");
+        break;
+    default: printf("[ft:reserved]");
     }
     if (rxinfo->pan_compression) {
         printf("[pan comp]");
@@ -39,21 +58,32 @@ void handle_rx(simrf_rx_info_t *rxinfo, uint8_t *rx_buffer) {
         printf("[security]");
     }
     switch (rxinfo->dest_addr_mode) {
-        case MAC_ADDRESS_MODE_NONE: printf("[dam:nopan,noaddr]"); break;
-        case MAC_ADDRESS_MODE_RESERVED: printf("[dam:reserved]"); break;
-        case MAC_ADDRESS_MODE_16: printf("[dam:16bit]");break;
-        case MAC_ADDRESS_MODE_64: printf("[dam:64bit]");break;
+    case MAC_ADDRESS_MODE_NONE: printf("[dam:nopan,noaddr]");
+        break;
+    case MAC_ADDRESS_MODE_RESERVED: printf("[dam:reserved]");
+        break;
+    case MAC_ADDRESS_MODE_16: printf("[dam:16bit]");
+        break;
+    case MAC_ADDRESS_MODE_64: printf("[dam:64bit]");
+        break;
     }
     switch (rxinfo->frame_version) {
-        case MAC_FRAME_VERSION_2003: printf("[fv:std2003]");break;
-        case MAC_FRAME_VERSION_2006: printf("[fv:std2006]");break;
-        default: printf("[fv:future]");break;
+    case MAC_FRAME_VERSION_2003: printf("[fv:std2003]");
+        break;
+    case MAC_FRAME_VERSION_2006: printf("[fv:std2006]");
+        break;
+    default: printf("[fv:future]");
+        break;
     }
     switch (rxinfo->src_addr_mode) {
-        case MAC_ADDRESS_MODE_NONE: printf("[sam:nopan,noaddr]"); break;
-        case MAC_ADDRESS_MODE_RESERVED: printf("[sam:reserved]"); break;
-        case MAC_ADDRESS_MODE_16: printf("[sam:16bit]");break;
-        case MAC_ADDRESS_MODE_64: printf("[sam:64bit]");break;
+    case MAC_ADDRESS_MODE_NONE: printf("[sam:nopan,noaddr]");
+        break;
+    case MAC_ADDRESS_MODE_RESERVED: printf("[sam:reserved]");
+        break;
+    case MAC_ADDRESS_MODE_16: printf("[sam:16bit]");
+        break;
+    case MAC_ADDRESS_MODE_64: printf("[sam:64bit]");
+        break;
     }
 
     printf("\nsequence: %d(%x)\n", rxinfo->sequence_number, rxinfo->sequence_number);
@@ -63,8 +93,8 @@ void handle_rx(simrf_rx_info_t *rxinfo, uint8_t *rx_buffer) {
     uint16_t dest_id = 0;
     uint16_t src_id = 0;
     if (rxinfo->dest_addr_mode == MAC_ADDRESS_MODE_16
-            && rxinfo->src_addr_mode == MAC_ADDRESS_MODE_16
-            && rxinfo->pan_compression) {
+        && rxinfo->src_addr_mode == MAC_ADDRESS_MODE_16
+        && rxinfo->pan_compression) {
         dest_pan = rx_buffer[i++];
         dest_pan |= rx_buffer[i++] << 8;
         dest_id = rx_buffer[i++];
@@ -75,7 +105,7 @@ void handle_rx(simrf_rx_info_t *rxinfo, uint8_t *rx_buffer) {
         printf("[d16:%x]", dest_id);
         printf("[s16:%x]", src_id);
         // TODO Can't move this into the library without doing more decoding in the library...
-        i += 2;  // as in the library, module seems to have two useless bytes after headers!
+        i += 2; // as in the library, module seems to have two useless bytes after headers!
     } else {
         printf("unimplemented address decoding!\n");
     }
--- examples/stm32f1/simrf_plat.c
+++ examples/stm32f1/simrf_plat.c
@@ -8,6 +8,7 @@
 #include <stdint.h>
 #include <string.h>
 
+#include <libopencm3/stm32/usart.h>
 #include <libopencm3/stm32/f1/rcc.h>
 #include <libopencm3/stm32/f1/gpio.h>
 #include <libopencm3/stm32/spi.h>
@@ -53,20 +54,69 @@ static void plat_reset(bool value) {
 }
 
 
-void init_spi(void) {
-    // TODO
-    spi_enable_software_slave_management(MRF_SPI);
-    spi_init_master()
-    spi_enable(MRF_SPI);
-}
-
-
 static uint8_t plat_spi_tx(uint8_t cData) {
     return spi_xfer(MRF_SPI, cData);
 }
 
+void clock_setup(void) {
+    /* Enable clocks on all the peripherals we are going to use. */
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_SPI1EN);
+    rcc_peripheral_enable_clock(&RCC_APB1ENR, RCC_APB1ENR_USART2EN);
+    // GPIOS... spi1 and usart2 are on port A
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPAEN);
+    // reset and interrupts on port C
+    rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPCEN);
+}
+
+void spi_setup(void) {
+    // SPI1 SCK
+    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO5);
+    // SPI1 MOSI
+    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO6);
+    // SPI ChipSelect
+    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO4);
+
+    /* Setup SPI parameters. */
+    spi_init_master(MRF_SPI, SPI_CR1_BAUDRATE_FPCLK_DIV_256, SPI_CR1_CPOL,
+        SPI_CR1_CPHA, SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
+    // stm32 spi sucksorz
+    spi_enable_software_slave_management(MRF_SPI);
+
+    /* Finally enable the SPI. */
+    spi_enable(MRF_SPI);
+}
+
+void usart_setup(void) {
+    // NEED GPIO FOR USARTS...
+    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_10_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO2);
+
+    /* Setup UART parameters. */
+    usart_set_baudrate(USART2, 19200);
+    usart_set_databits(USART2, 8);
+    usart_set_stopbits(USART2, USART_STOPBITS_1);
+    usart_set_parity(USART2, USART_PARITY_NONE);
+    usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+    usart_set_mode(USART2, USART_MODE_TX);
+
+    /* Finally enable the USART. */
+    usart_enable(USART2);
+}
+
+void gpio_setup(void) {
+    // MRF reset pin
+    gpio_set_mode(MRF_RESET_PORT, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, MRF_RESET_PIN);
+    // MRF interrupt pin
+    gpio_set_mode(GPIOC, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO0);
+
+}
+
+
 void platform_simrf_init(void) {
-    init_spi();
+    rcc_clock_setup_in_hse_8mhz_out_24mhz();
+    clock_setup();
+    gpio_setup();
+    usart_setup();
+    spi_setup();
     
     struct simrf_platform plat;
     memset(&plat, 0, sizeof(plat));
