3f29ca6b2957e4db96b9afc5df5665c3e5bc531a
Karl Palsson <karlp@tweak.net.au>
2012-08-24 22:30:16 +0000

    Fix SPI!
    Now, interrupts!

--- examples/stm32f1/simrf_plat.c
+++ examples/stm32f1/simrf_plat.c
@@ -71,14 +71,15 @@ void spi_setup(void) {
     // SPI1 MOSI
     gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_SPI1_MOSI);
     // SPI ChipSelect
-    gpio_set_mode(MRF_SELECT_PORT, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, MRF_SELECT_PIN);
+    gpio_set_mode(MRF_SELECT_PORT, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, MRF_SELECT_PIN);
     gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO_SPI1_MISO);
 
     /* Setup SPI parameters. */
-    spi_init_master(MRF_SPI, SPI_CR1_BAUDRATE_FPCLK_DIV_256, SPI_CR1_CPOL,
-        SPI_CR1_CPHA, SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
+    spi_init_master(MRF_SPI, SPI_CR1_BAUDRATE_FPCLK_DIV_8, SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
+        SPI_CR1_CPHA_CLK_TRANSITION_1, SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
     /* Ignore the stupid NSS pin. */
     spi_enable_software_slave_management(MRF_SPI);
+    spi_enable_ss_output(MRF_SPI);
     spi_set_nss_high(MRF_SPI);
 
     /* Finally enable the SPI. */
